6c607d33a1e3ad1f02147cb014ee3d7110509c98
0317trz <2023091203017@std.uestc.edu.cn>
2024-12-18 22:03:06 +0800

    Changes

---
+++ Web/Background/Background.vue
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+impory 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI);
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Background",
+}
+</script>
